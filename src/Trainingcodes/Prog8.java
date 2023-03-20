9fa85d326aa6b145d07c60c4e5e16c0a266529e7
pc <pc@DESKTOP-UAEG96R>
2023-03-20 23:50:46 +0530

    Added

---
+++ src/Trainingcodes/Prog8.java
@@ -0,0 +1,14 @@
+package Trainingcodes;
+
+public class Prog8 {
+	public static void main(String[] args) {
+		int i=1;
+		while(i<=5) {
+			System.out.println("Hello world");
+			i++;
+		}
+		
+		
+	}
+
+}
