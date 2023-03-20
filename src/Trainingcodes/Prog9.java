2b98074b767f8634413787ef55ea40d771017143
pc <pc@DESKTOP-UAEG96R>
2023-03-20 23:51:39 +0530

    Added successfully

---
+++ src/Trainingcodes/Prog9.java
@@ -0,0 +1,14 @@
+package Trainingcodes;
+
+public class Prog9 {
+	public static void main(String[] args) {
+		int i=1;
+		do {
+			System.out.println("Hello good noon");
+			i++;
+		}while(i<=5);
+		}
+		
+	}
+
+
