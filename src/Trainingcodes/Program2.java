bf912ec7af21e9ff062f968265b205a77e9bef22
pc <pc@DESKTOP-UAEG96R>
2023-03-20 23:52:54 +0530

    Added

---
+++ src/Trainingcodes/Program2.java
@@ -0,0 +1,18 @@
+package Trainingcodes;
+import java.util.Scanner;
+public class Program2 {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the value of n:");
+		int n= sc.nextInt();
+		int count=n*n;
+	
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++){
+		System.out.print( count+" ");
+		count--;
+	}
+System.out.println();
+}
+}
+}
