024a390ff3a79425e601f9ce26382df06a17efd2
pc <pc@DESKTOP-UAEG96R>
2023-03-20 23:49:08 +0530

    Added

---
+++ src/Trainingcodes/Prog6.java
@@ -0,0 +1,12 @@
+package Trainingcodes;
+
+public class Prog6 {
+public static void main(String[] args) {
+	int count=1;
+	for(int i=1;i<=5;i++) {
+		System.out.print(count+" ");
+		count=count+5;
+	}
+}
+}
+
