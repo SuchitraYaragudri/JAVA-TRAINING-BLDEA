eeb1ec9aa75f44d133c60a2bf5ad07dbd1bca3a3
pc <pc@DESKTOP-UAEG96R>
2023-03-20 23:49:55 +0530

    Added successfully

---
+++ src/Trainingcodes/Prog7.java
@@ -0,0 +1,14 @@
+package Trainingcodes;
+
+public class Prog7 {
+public static void main(String[] args) {
+	int count=25;
+	for(int i=1;i<=5;i++) {
+		System.out.print(count+" ");
+		count=count-5;
+	}
+}
+}
+
+
+
