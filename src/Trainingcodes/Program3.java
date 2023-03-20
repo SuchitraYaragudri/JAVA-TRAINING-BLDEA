43a38f34854ca2f07b9eb6678e91019f96a05bfe
pc <pc@DESKTOP-UAEG96R>
2023-03-20 23:53:38 +0530

    Successfully Added

---
+++ src/Trainingcodes/Program3.java
@@ -0,0 +1,18 @@
+package Trainingcodes;
+import java.util.Scanner;
+public class Program3 {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the value of n:");
+		int n= sc.nextInt();
+		int count;
+		for(int i=1;i<=5;i++) {
+			count=i;
+			for(int j=1;j<=5;j++){
+		System.out.print( count+" ");
+		count=count+5;
+	}
+System.out.println();
+}
+}
+}
