88ff60accc07df3da119ae64a27e14051cef4eaa
pc <pc@DESKTOP-UAEG96R>
2023-03-21 00:00:05 +0530

    Added

---
+++ src/Trainingcodes/UniversalPat4.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package Trainingcodes;
+import java.util.Scanner;
+/**
+ * @author pc
+ *
+ */
+public class UniversalPat4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the value of number n:");
+int n=sc.nextInt();
+for(int i=0;i<n;i++) {
+	for(int j=0;j<n;j++) {
+		if(j==0 && i<=(n/2) || (i-j==(n/2) ||i+j==(n-1)+(n/2)||(j==(n-1) && i<=(n/2)))){
+			System.out.print("* ");
+			
+		}
+		else {
+			System.out.print("  ");
+		}
+}
+	
+	for(int j=0;j<n;j++) {
+		if(j==0 || i+j==(n/2) || i-j==(n/2)) {
+			System.out.print("* ");
+		}
+		else {
+			System.out.print("  ");
+		}
+	}
+    System.out.println("  ");
+}
+}
+}
